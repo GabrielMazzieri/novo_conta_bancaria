47d1fea9e20438e5ca7324f7d44561378eb4963b
gabriel <gabrielnmyzy@gmail.com>
2025-09-18 11:16:11 -0300

    conta bancaria - 18/09

--- src/main/java/com/senai/conta_bancaria/application/service/ClienteService.java
+++ src/main/java/com/senai/conta_bancaria/application/service/ClienteService.java
@@ -2,6 +2,7 @@ package com.senai.conta_bancaria.application.service;
 
 import com.senai.conta_bancaria.application.dto.ClienteRegistroDTO;
 import com.senai.conta_bancaria.application.dto.ClienteResponseDTO;
+import com.senai.conta_bancaria.domain.entity.Conta;
 import com.senai.conta_bancaria.domain.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
