1bdc15f7d33ab0dcd4cdae0a05634d09ef1fc0ae
gabriel <gabrielnmyzy@gmail.com>
2025-09-25 09:24:10 -0300

    conta bancaria - 25/09

---
+++ src/main/java/com/senai/conta_bancaria/application/service/ClienteService.java
@@ -0,0 +1,60 @@
+package com.senai.conta_bancaria.application.service;
+
+import com.senai.conta_bancaria.application.dto.ClienteAtualizadoDTO;
+import com.senai.conta_bancaria.application.dto.ClienteRegistroDTO;
+import com.senai.conta_bancaria.application.dto.ClienteResponseDTO;
+import com.senai.conta_bancaria.domain.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    public ClienteResponseDTO registrarCliente(ClienteRegistroDTO dto) {
+
+        var cliente = repository.findByCpfAndAtivoTrue(dto.cpf()).orElseGet(
+                () -> repository.save(dto.toEntity())
+                );
+        var contas = cliente.getContas();
+        var novaConta = dto.contaDTO().toEntity(cliente);
+
+        boolean jaTemTipo = contas.stream().anyMatch(
+                c -> c.getClass().equals(novaConta.getClass()) && c.isAtiva()
+                );
+        if (jaTemTipo)
+            throw new RuntimeException("Cliente já possui uma conta desse tipo.");
+
+        cliente.getContas().add(novaConta);
+
+        return ClienteResponseDTO.fromEntity(repository.save(cliente));
+    }
+
+    public List<ClienteResponseDTO> listarClientesAtivos() {
+        return repository.findAllByAtivoTrue().stream()
+                .map(ClienteResponseDTO::fromEntity)
+                .toList();
+    }
+
+    public ClienteResponseDTO buscarClienteAtivoPorCpf(String cpf) {
+        var cliente = repository.findByCpfAndAtivoTrue(cpf).orElseThrow(
+                () -> new RuntimeException("Cliente não encontrado.")
+        );
+        return ClienteResponseDTO.fromEntity(cliente);
+    }
+
+    public ClienteResponseDTO atualizarCliente(String cpf, ClienteAtualizadoDTO dto) {
+        var cliente = repository.findByCpfAndAtivoTrue(cpf).orElseThrow(
+                () -> new RuntimeException("Cliente não encontrado.")
+        );
+
+        cliente.setNome(dto.nome());
+        cliente.setCpf(dto.cpf());
+
+        return ClienteResponseDTO.fromEntity(repository.save(cliente));
+    }
+}
---
+++ src/main/java/com/senai/conta_bancaria/domain/entity/ContaPoupanca.java
@@ -0,0 +1,28 @@
+package com.senai.conta_bancaria.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@DiscriminatorValue("POUPANCA")
+public class ContaPoupanca extends Conta{
+
+    @Column(precision = 5)
+    private BigDecimal rendimento;
+
+    @Override
+    public String getTipo() {
+        return "POUPANCA";
+    }
+}
